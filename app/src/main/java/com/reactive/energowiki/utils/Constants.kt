b40b0a673cdb8c0c32643eed3c6917c7ac60d5fe
AbrusDev <abrusdev@gmail.com>
2020-07-30 23:10:00 +0500

    changes with grandle

--- app/src/main/java/com/reactive/energowiki/utils/Constants.kt
+++ app/src/main/java/com/reactive/energowiki/utils/Constants.kt
@@ -4,10 +4,9 @@ import com.reactive.energowiki.BuildConfig
 
 object Constants {
 
-    const val BASE_URL = BuildConfig.BASE_URL
+    const val BASE_URL =BuildConfig.BASE_URL
     const val TIMEOUT = 10.toLong()
 
-    const val IS_REGISTER_KEY = "register"
 }
 
 data class KeyValue(val key: String, val value: String)
