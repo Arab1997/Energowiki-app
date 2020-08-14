faa6821e9065a2832c20556d4c1c687b08e71f6a
AbrusDev <abrusdev@gmail.com>
2020-08-14 15:06:54 +0500

    new calculation screen

---
+++ app/src/main/java/com/reactive/energowiki/ui/screens/calculator/VoltageCalcScreen.kt
@@ -0,0 +1,10 @@
+package com.reactive.energowiki.ui.screens.calculator
+
+import com.reactive.energowiki.R
+import com.reactive.energowiki.base.BaseFragment
+
+class VoltageCalcScreen : BaseFragment(R.layout.screen_calc_voltage){
+    override fun initialize() {
+    }
+
+}
