b9433f32abca4252706f7ea6099e8f4708e32a4d
AbrusDev <abrusdev@gmail.com>
2020-08-15 01:20:26 +0500

    UX calculator screens

--- app/src/main/java/com/reactive/energowiki/ui/screens/calculator/ContinousCurrCalcScreen.kt
+++ app/src/main/java/com/reactive/energowiki/ui/screens/calculator/ContinousCurrCalcScreen.kt
@@ -1,11 +1,122 @@
 package com.reactive.energowiki.ui.screens.calculator
 
+import android.annotation.SuppressLint
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.reactive.energowiki.R
 import com.reactive.energowiki.base.BaseFragment
+import kotlinx.android.synthetic.main.content_header.*
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.*
 
 class ContinousCurrCalcScreen : BaseFragment(R.layout.screen_calc_continous_curr){
-    override fun initialize() {
 
+    private val spinValues = arrayListOf<ArrayList<String>>()
+
+    override fun initialize() {
+        initViews()
+
+        initClicks()
+
+        initSpinners()
+
+        initEditTexts()
+    }
+
+    private fun initSpinners(){
+        spinValues.add(arrayListOf("пОм", "нОм", "мкОм", "мОм", "Ом", "кОм", "МОм", "ГОм"))
+        //todo: Khoshimova
+        spinValues.add(arrayListOf("В"))
+        val aa1: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[0])
+        aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = aa1
+        val aa2: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[1])
+        aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.adapter = aa2
+
+
+        spinner1.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //todo: Khoshimova koef
+                initCalculation()
+            }
+        }
+
+        spinner2.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //todo: Khoshimova
+                initCalculation()
+            }
+        }
+
+
+    }
+
+    private fun initViews() {
+
+        header.text = "Закон Ома постоянного тока"
+    }
+
+    private fun initClicks() {
+
+        close.setOnClickListener { finishFragment() }
+
+        clearBtn.setOnClickListener {
+            input1.text?.clear()
+            input2.text?.clear()
+        }
+
+        resultBtn.setOnClickListener {
+            initCalculation()
+        }
+    }
+
+    private fun initEditTexts() {
+        input2.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                initCalculation()
+            }
+
+        })
+    }
+
+
+    private fun initCalculation() {
+        val result=0.0
+        //todo: Khoshimova
+
+        showResult(result)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showResult(res: Double) {
+        result.text = "$res м"
     }
 
 }
