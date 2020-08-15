60d0d6f0ae4246d6adffae79c7a4870d83c8f371
Leyla2627 <leyla2627>
2020-08-15 08:54:57 +0300

    finish calculation

--- app/src/main/java/com/reactive/energowiki/ui/screens/calculator/AlternativeCurrCalcScreen.kt
+++ app/src/main/java/com/reactive/energowiki/ui/screens/calculator/AlternativeCurrCalcScreen.kt
@@ -10,9 +10,15 @@ import com.reactive.energowiki.R
 import com.reactive.energowiki.base.BaseFragment
 import kotlinx.android.synthetic.main.content_header.*
 import kotlinx.android.synthetic.main.screen_calc_alternative_curr.*
+import kotlin.math.pow
 
-class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_curr){
+class AlternativeCurrCalcScreen : BaseFragment(R.layout.screen_calc_alternative_curr) {
     private val spinValues = arrayListOf<ArrayList<String>>()
+    private var koef1: Double = 1.0
+    private var koef2: Double = 1.0
+    private var koef3: Double = 1.0
+    private var koef4: Double = 1.0
+
 
     override fun initialize() {
         initViews()
@@ -24,25 +30,28 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
         initEditTexts()
     }
 
-    private fun initSpinners(){
+    private fun initSpinners() {
         spinValues.add(arrayListOf("пОм", "нОм", "мкОм", "мОм", "Ом", "кОм", "МОм", "ГОм"))
-        //todo: Khoshimova
-        spinValues.add(arrayListOf("W"))
-        val aa1: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[0])
+        spinValues.add(arrayListOf("пВ", "нВ", "мкВ", "мВ", "В", "кВ", "МВ", "ГВ"))
+        val aa1: ArrayAdapter<String> =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[0])
         aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner1.adapter = aa1
-        val aa2: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[1])
+        val aa2: ArrayAdapter<String> =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[0])
         aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner2.adapter = aa2
-        val aa3: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[1])
+        val aa3: ArrayAdapter<String> =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[0])
         aa3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner3.adapter = aa3
-        val aa4: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[1])
+        val aa4: ArrayAdapter<String> =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[1])
         aa4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner4.adapter = aa4
 
 
-        spinner1.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
 
@@ -52,12 +61,23 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
                 position: Int,
                 id: Long
             ) {
-                //todo: Khoshimova koef
+                koef1 = 1.0
+                koef1 *= when (position) {
+                    0 -> 10.0.pow(-12.0)
+                    1 -> 10.0.pow(-9.0)
+                    2 -> 10.0.pow(-6.0)
+                    3 -> 10.0.pow(-3.0)
+                    4 -> 1.0
+                    5 -> 10.0.pow(3)
+                    6 -> 10.0.pow(6)
+                    7 -> 10.0.pow(9)
+                    else -> 1.0
+                }
                 initCalculation()
             }
         }
 
-        spinner2.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
 
@@ -67,12 +87,23 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
                 position: Int,
                 id: Long
             ) {
-                //todo: Khoshimova
+                koef2 = 1.0
+                koef2 *= when (position) {
+                    0 -> 10.0.pow(-12.0)
+                    1 -> 10.0.pow(-9.0)
+                    2 -> 10.0.pow(-6.0)
+                    3 -> 10.0.pow(-3.0)
+                    4 -> 1.0
+                    5 -> 10.0.pow(3)
+                    6 -> 10.0.pow(6)
+                    7 -> 10.0.pow(9)
+                    else -> 1.0
+                }
                 initCalculation()
             }
         }
 
-        spinner3.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+        spinner3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
 
@@ -82,12 +113,23 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
                 position: Int,
                 id: Long
             ) {
-                //todo: Khoshimova
+                koef3 = 1.0
+                koef3 *= when (position) {
+                    0 -> 10.0.pow(-12.0)
+                    1 -> 10.0.pow(-9.0)
+                    2 -> 10.0.pow(-6.0)
+                    3 -> 10.0.pow(-3.0)
+                    4 -> 1.0
+                    5 -> 10.0.pow(3)
+                    6 -> 10.0.pow(6)
+                    7 -> 10.0.pow(9)
+                    else -> 1.0
+                }
                 initCalculation()
             }
         }
 
-        spinner4.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+        spinner4.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
 
@@ -97,7 +139,18 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
                 position: Int,
                 id: Long
             ) {
-                //todo: Khoshimova
+                koef4 = 1.0
+                koef4 *= when (position) {
+                    0 -> 10.0.pow(-12.0)
+                    1 -> 10.0.pow(-9.0)
+                    2 -> 10.0.pow(-6.0)
+                    3 -> 10.0.pow(-3.0)
+                    4 -> 1.0
+                    5 -> 10.0.pow(3)
+                    6 -> 10.0.pow(6)
+                    7 -> 10.0.pow(9)
+                    else -> 1.0
+                }
                 initCalculation()
             }
         }
@@ -115,6 +168,8 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
         clearBtn.setOnClickListener {
             input1.text?.clear()
             input2.text?.clear()
+            input3.text?.clear()
+            input4.text?.clear()
         }
 
         resultBtn.setOnClickListener {
@@ -123,7 +178,49 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
     }
 
     private fun initEditTexts() {
+        input1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                initCalculation()
+            }
+
+        })
         input2.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                initCalculation()
+            }
+
+        })
+        input3.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                initCalculation()
+            }
+
+        })
+        input4.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
             }
@@ -141,15 +238,17 @@ class AlternativeCurrCalcScreen: BaseFragment(R.layout.screen_calc_alternative_c
 
 
     private fun initCalculation() {
-        val result=0.0
-        //todo: Khoshimova
-
-        showResult(result)
+        val R = input1.text.toString().let { if (it.isEmpty()) 0.0 else it.toDouble() }
+        val w_l = input2.text.toString().let { if (it.isEmpty()) 0.0 else it.toDouble() }
+        val w_c = input3.text.toString().let { if (it.isEmpty()) 1.0 else it.toDouble() }
+        val U = input4.text.toString().let { if (it.isEmpty()) 0.0 else it.toDouble() }
+        val result = U / (((koef1 * R).pow(2) + (koef3 * w_c - 1 / (w_l * koef2)).pow(2)).pow(0.5))
+        showResult(result * koef4)
     }
 
     @SuppressLint("SetTextI18n")
     private fun showResult(res: Double) {
-        result.text = "$res м"
+        result.text = "$res А"
     }
 
 
--- app/src/main/java/com/reactive/energowiki/ui/screens/calculator/ContinousCurrCalcScreen.kt
+++ app/src/main/java/com/reactive/energowiki/ui/screens/calculator/ContinousCurrCalcScreen.kt
@@ -9,12 +9,20 @@ import android.widget.ArrayAdapter
 import com.reactive.energowiki.R
 import com.reactive.energowiki.base.BaseFragment
 import kotlinx.android.synthetic.main.content_header.*
-import kotlinx.android.synthetic.main.screen_calc_continous_curr.*
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.clearBtn
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.input1
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.input2
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.result
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.resultBtn
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.spinner1
+import kotlinx.android.synthetic.main.screen_calc_continous_curr.spinner2
+import kotlin.math.pow
 
 class ContinousCurrCalcScreen : BaseFragment(R.layout.screen_calc_continous_curr){
 
     private val spinValues = arrayListOf<ArrayList<String>>()
-
+    private var koef1: Double = 1.0
+    private var koef2: Double = 1.0
     override fun initialize() {
         initViews()
 
@@ -27,8 +35,7 @@ class ContinousCurrCalcScreen : BaseFragment(R.layout.screen_calc_continous_curr
 
     private fun initSpinners(){
         spinValues.add(arrayListOf("пОм", "нОм", "мкОм", "мОм", "Ом", "кОм", "МОм", "ГОм"))
-        //todo: Khoshimova
-        spinValues.add(arrayListOf("В"))
+        spinValues.add(arrayListOf("пВ", "нВ", "мкВ", "мВ", "В", "кВ", "МВ", "ГВ"))
         val aa1: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinValues[0])
         aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner1.adapter = aa1
@@ -47,7 +54,18 @@ class ContinousCurrCalcScreen : BaseFragment(R.layout.screen_calc_continous_curr
                 position: Int,
                 id: Long
             ) {
-                //todo: Khoshimova koef
+                koef1 = 1.0
+                koef1 *= when (position) {
+                    0 -> 10.0.pow(-12.0)
+                    1 -> 10.0.pow(-9.0)
+                    2 -> 10.0.pow(-6.0)
+                    3 -> 10.0.pow(-3.0)
+                    4 -> 1.0
+                    5 -> 10.0.pow(3)
+                    6 -> 10.0.pow(6)
+                    7 -> 10.0.pow(9)
+                    else -> 1.0
+                }
                 initCalculation()
             }
         }
@@ -61,8 +79,18 @@ class ContinousCurrCalcScreen : BaseFragment(R.layout.screen_calc_continous_curr
                 view: View?,
                 position: Int,
                 id: Long
-            ) {
-                //todo: Khoshimova
+            ) { koef1 = 1.0
+                koef1 *= when (position) {
+                    0 -> 10.0.pow(-12.0)
+                    1 -> 10.0.pow(-9.0)
+                    2 -> 10.0.pow(-6.0)
+                    3 -> 10.0.pow(-3.0)
+                    4 -> 1.0
+                    5 -> 10.0.pow(3)
+                    6 -> 10.0.pow(6)
+                    7 -> 10.0.pow(9)
+                    else -> 1.0
+                }
                 initCalculation()
             }
         }
@@ -90,6 +118,20 @@ class ContinousCurrCalcScreen : BaseFragment(R.layout.screen_calc_continous_curr
     }
 
     private fun initEditTexts() {
+        input1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                initCalculation()
+            }
+
+        })
         input2.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
@@ -108,15 +150,16 @@ class ContinousCurrCalcScreen : BaseFragment(R.layout.screen_calc_continous_curr
 
 
     private fun initCalculation() {
-        val result=0.0
-        //todo: Khoshimova
+        val R = input1.text.toString().let { if (it.isEmpty()) 0.0 else it.toDouble() }
+        val U = input2.text.toString().let { if (it.isEmpty()) 0.0 else it.toDouble() }
+        val result=U/R
 
-        showResult(result)
+        showResult(result*koef2/koef1)
     }
 
     @SuppressLint("SetTextI18n")
     private fun showResult(res: Double) {
-        result.text = "$res м"
+        result.text = "$res А"
     }
 
 }
