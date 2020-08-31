f64746831ac8756a6f846b98f3d2fd2855f2f520
Abdulloh <maxmudovabdullo97@gmail.com>
2020-08-31 12:27:59 +0500

    add Pue category to energowiki

---
+++ app/src/main/java/com/reactive/energowiki/ui/adapters/PueAdapter.kt
@@ -0,0 +1,79 @@
+package com.reactive.energowiki.ui.adapters
+
+import com.reactive.energowiki.R
+import com.reactive.energowiki.base.BaseAdapter
+import com.reactive.energowiki.ui.screens.PueData
+import com.reactive.energowiki.ui.screens.PueMenus
+import com.reactive.energowiki.ui.screens.calculator.CalculatorData
+import com.reactive.energowiki.ui.screens.calculator.CalculatorMenus
+import com.reactive.energowiki.utils.common.ViewHolder
+import kotlinx.android.synthetic.main.item_calculator.view.*
+
+class PueAdapter(private val type: PueData, private val listener: (Any) -> Unit) :
+    BaseAdapter<Any>(R.layout.item_pue) {
+    override fun bindViewHolder(holder: ViewHolder, data: Any) {
+        when (type.type) {
+            PueMenus.BASICS -> when (data) {
+                1 -> holder.itemView.name.text = "Допустимые силы тока для провода"
+                2 -> holder.itemView.name.text = "Калкулятор размера провода"
+                3 -> holder.itemView.name.text = "Защита двигателя"
+                4 -> holder.itemView.name.text = "Максимальная длина проводника цепи"
+                5 -> holder.itemView.name.text = "Максимальная мощность цепи"
+            }
+            PueMenus.CAPACITY -> when (data) {
+                1 -> holder.itemView.name.text = "Соединение конденсаторов"
+                2 -> holder.itemView.name.text = "Сопротивление емкости переменному току"
+                3 -> holder.itemView.name.text =
+                    "Конденсатор для пуска трехфазного двигателя в однофазной сети"
+                4 -> holder.itemView.name.text = "Емкостной делитель напряжение"
+                5 -> holder.itemView.name.text = "Заряд и энергия корденсатора"
+                6 -> holder.itemView.name.text = "Заряд и энергия конденсатора"
+                7 -> holder.itemView.name.text = "Декодировать в буквенно-цифровую маркировку"
+                8 -> holder.itemView.name.text = "Кодировать в буквенно-цифровую маркировку"
+                9 -> holder.itemView.name.text = "Гасящий конденсатор"
+            }
+            PueMenus.CONDUCTOR -> when (data) {
+                1 -> holder.itemView.name.text = "Сопротивление проводника"
+                2 -> holder.itemView.name.text = "Длина проводника"
+                3 -> holder.itemView.name.text = "Сечение проводника"
+                4 -> holder.itemView.name.text = "Расчёт потерь напряжения"
+                5 -> holder.itemView.name.text = "Удельное сопротивление и проводимость"
+                6 -> holder.itemView.name.text = "Максимальная длина проводника"
+            }
+            PueMenus.ENGINE -> when (data) {
+                1 -> holder.itemView.name.text = "Ток электродвигателя"
+                2 -> holder.itemView.name.text = "Активная мощность"
+                3 -> holder.itemView.name.text = "Полная мощность ( 3 фазы)"
+                4 -> holder.itemView.name.text = "Коеффициент мощности (3 фазы)"
+                5 -> holder.itemView.name.text = "Кпд электродвигателя"
+                6 -> holder.itemView.name.text = "Скольжение электродвигателя"
+                7 -> holder.itemView.name.text = "Крутящий  момент"
+                8 -> holder.itemView.name.text =
+                    "Единичная компенсация мощноности трехнофазного электродвигателя"
+                9 -> holder.itemView.name.text = "Мощность элекродвигателя для центробежного насоса"
+                10 -> holder.itemView.name.text =
+                    "Мощность электродвигателя для поршневого компрессора"
+                11 -> holder.itemView.name.text = "Мощность электродвигателя для вентилятро"
+                12 -> holder.itemView.name.text =
+                    "Асимметрия напряжений трехфазного элеткродвигателя"
+                13 -> holder.itemView.name.text = "Ассиметрия токов трехфазного электродвигателя"
+            }
+            PueMenus.CABEL -> when (data) {
+                1 -> holder.itemView.name.text = "Количество кабелей, которые помещаются в трубу"
+                2 -> holder.itemView.name.text = "Емкость кабельного барабана"
+                3 -> holder.itemView.name.text = "Остаток кабеля на барабане"
+                4 -> holder.itemView.name.text = "Индуктивное сопротивление кабеля"
+                5 -> holder.itemView.name.text = "Все металла в кабеле"
+                6 -> holder.itemView.name.text = "Все кабеля по формулам"
+                7 -> holder.itemView.name.text = "Цветовая кодировка проводов"
+                8 -> holder.itemView.name.text = "Длина кабеля с учетом провиса"
+                9 -> holder.itemView.name.text = "Потери мощности в кабеле"
+                10 -> holder.itemView.name.text = "Минимальный размер защитного проводника"
+            }
+        }
+
+        holder.itemView.setOnClickListener {
+            listener.invoke(data)
+        }
+    }
+}
---
+++ app/src/main/java/com/reactive/energowiki/ui/screens/PueFragment.kt
@@ -0,0 +1,94 @@
+package com.reactive.energowiki.ui.screens
+
+import com.reactive.energowiki.R
+import com.reactive.energowiki.base.BaseFragment
+import com.reactive.energowiki.ui.adapters.PueAdapter
+import com.reactive.energowiki.ui.screens.calculator.cabel.*
+import com.reactive.energowiki.ui.screens.calculator.capacity.Capacity1Screen
+import com.reactive.energowiki.ui.screens.calculator.capacity.Capacity2Screen
+import com.reactive.energowiki.ui.screens.calculator.engine.*
+import com.reactive.energowiki.ui.screens.pue.*
+import com.reactive.energowiki.utils.extensions.toast
+import kotlinx.android.synthetic.main.fragment_pue.*
+
+class PueFragment : BaseFragment(R.layout.fragment_pue) {
+
+    companion object {
+        private lateinit var data: PueData
+        fun newInstance(data: PueData): PueFragment {
+            this.data = data
+            return PueFragment()
+        }
+    }
+
+    override fun initialize() {
+        recyclerPue.adapter =
+            PueAdapter(
+                data ?: PueData("Основы", PueMenus.BASICS)
+            ) {
+                when (data.type) {
+                    PueMenus.BASICS -> when (it) {
+                        1 -> addFragment(AllowableAmpacitiesForWireScreen())
+                        2 -> addFragment(WireSizeScreen())
+                        3 -> addFragment(MotorProtectionScreen())
+                        4 -> addFragment(MaximumCircuitConductorLengthScreen())
+                        5 -> addFragment(MaximumCircuitAmpacityScreen())
+
+                        else -> toast(requireContext(), "inDevelopment")
+                    }
+                    PueMenus.CAPACITY -> when (it) {
+                        1 -> addFragment(Capacity1Screen())
+                        2 -> addFragment(Capacity2Screen())
+                        else -> toast(requireContext(), "inDevelopment")
+                    }
+                    PueMenus.ENGINE -> when (it) {
+                        1 -> addFragment(Engine1Screen())
+                        2 -> addFragment(Engine2Screen())
+                        3 -> addFragment(Engine3Screen())
+                        4 -> addFragment(Engine4Screen())
+                        5 -> addFragment(Engine5Screen())
+                        6 -> addFragment(Engine6Screen())
+                        7 -> addFragment(Engine7Screen())
+                        8 -> addFragment(Engine8Screen())
+                        9 -> addFragment(Engine9Screen())
+                        10 -> addFragment(Engine10Screen())
+                        11 -> addFragment(Engine11Screen())
+                        12 -> addFragment(Engine12Screen())
+                        13 -> addFragment(Engine13Screen())
+                        else -> toast(requireContext(), "inDevelopment")
+                    }
+                    PueMenus.CABEL -> when (it) {
+                        1 -> addFragment(Cabel1Screen())
+                        2 -> addFragment(Cabel2Screen())
+                        3 -> addFragment(Cabel3Screen())
+                        4 -> addFragment(Cabel4Screen())
+                        5 -> addFragment(Cabel5Screen())
+                        6 -> addFragment(Cabel6Screen())
+                        7 -> addFragment(Cabel7Screen())
+                        8 -> addFragment(Cabel8Screen())
+                        9 -> addFragment(Cabel9Screen())
+                        10 -> addFragment(Cabel10Screen())
+                        else -> toast(requireContext(), "inDevelopment")
+                    }
+                }
+
+            }.apply {
+                setData(
+                    when (data.type) {
+                        PueMenus.BASICS -> arrayListOf<Any>(1, 2, 3, 4, 5)
+                        PueMenus.CAPACITY -> arrayListOf<Any>(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                        PueMenus.CONDUCTOR -> arrayListOf<Any>(1, 2, 3, 4, 5, 6)
+                        PueMenus.ENGINE -> arrayListOf<Any>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13)
+                        PueMenus.CABEL -> arrayListOf<Any>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                        else -> arrayListOf<Any>(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                    }
+                )
+            }
+    }
+}
+
+data class PueData(val name: String, val type: PueMenus)
+
+enum class PueMenus {
+    BASICS, CAPACITY, CONDUCTOR, ENGINE, CABEL
+}
